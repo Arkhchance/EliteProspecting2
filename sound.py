da61f8a81efccca6f5924e7bab791c8201579b33
Arkhchance <arkhchance@protonmail.com>
2020-04-24 14:04:03 +0200

    fix sound bug, now search in correct path

--- sound.py
+++ sound.py
@@ -5,18 +5,21 @@ from playsound import playsound
 
 class sound():
     def __init__(self):
-        self.sound_file = os.path.dirname(os.path.realpath(__file__))
-        self.sound_file += "/EliteProspecting2sound/*"
+        self.sound_file = os.getcwd() + "/EliteProspecting2sound/*"
         fileList = glob.glob(self.sound_file)
-        self.sound_file = fileList[0]
-        print(fileList)
+        self.isFile = True
+        try:
+            self.sound_file = fileList[0]
+        except IndexError :
+            print("no file found")
+            self.isFile = False
 
     def play(self):
-        threading.Thread(target=self.playSound).start()
+        if self.isFile:
+            threading.Thread(target=self.playSound).start()
 
     def playSound(self):
         try:
-            print("playing ",self.sound_file)
             playsound(self.sound_file)
         except Exception as e :
             print("there was an error playing sound file ", e, self.sound_file)
